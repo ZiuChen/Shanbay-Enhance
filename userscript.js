af9f45fcc718af19b09affa5c5505c07fb1d6cce
ZiuChen <64892985+ZiuChen@users.noreply.github.com>
2022-01-02 22:08:06 +0800

    description toggle

--- userscript.js
+++ userscript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            扇贝单词增强
 // @name:en         Shanbay Enhance
-// @description     扇贝单词功能增强，带来更好的背单词体验。❤单词释义隐藏和查看功能
+// @description     扇贝单词功能增强，带来更好的背单词体验。❤单词总结释义遮挡
 // @version         0.1
 // @author          Ziu
 // @namespace       https://greasyfork.org/zh-CN/users/605474
